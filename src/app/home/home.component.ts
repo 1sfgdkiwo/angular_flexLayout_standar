562bb4dc7034c316f6a52631ea4c8cf7ef9bf30c
1sfgdkiwo <rumahkode.gmail.com>
2023-03-08 14:36:48 +0700

    angular_flex_layout_standar

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  menuDrop(event:any) {
+    (<HTMLInputElement>document.getElementById("myDropdown")).classList.toggle("show");
+       if (!event.target.matches('.dropbtn')) {
+         var dropdowns = document.getElementsByClassName("dropdown-content");
+         var i;
+         for (i = 0; i < dropdowns.length; i++) {
+           var openDropdown = dropdowns[i];
+           if (openDropdown.classList.contains('show')) {
+             openDropdown.classList.remove('show');
+           }
+         }
+       }
+     }
+   
+}
